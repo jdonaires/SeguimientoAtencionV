d404165cbcacb7efbf602a3b10191c60e572f2d8
gilbertoalexander <alexander_96.05@outlook.com>
2018-11-21 02:17:55 -0500

    DiseñoPrincipal

--- Capas/index.php
+++ Capas/index.php
@@ -1,5 +1,22 @@
 <?php
 ?>
 <html>
-<a href="DESIGNER/frmPersona.php">Persona</a>
+<head>
+  <title>HOSPITAL</title>
+</head>
+<link rel="stylesheet" href="CSS/principal.css">
+
+<header>
+<nav class="menu">
+    <ul>
+        <li><a href="DESIGNER/frmPersona.php">Persona</a></li>
+        <li><a href=""><i class=""></i>Inicio</a></li>
+        <li><a href=""><i class=""></i>Lista</a></li>
+        <li><a href=""><i class=""></i>Nuevo Registro</a></li>
+        <li><a href=""><i class=""></i>Nueva Cita</a></li>
+        <li><a href=""><i class=""></i>Ayuda</a></li>
+    </ul>
+</nav>
+</header>
+
 </html>
