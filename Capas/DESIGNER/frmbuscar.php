539f4525b454dc86d49c948fa570b3df9040004a
AlexanderPachasA <alexander_jepa_30@hotmail.com>
2018-11-21 00:21:53 -0500

    Modificar formulario buscar
    Se modifica el formulario para probar la ejecucion del codigo

---
+++ Capas/DESIGNER/frmbuscar.php
@@ -0,0 +1,16 @@
+<?php
+require_once('../BOL/persona.php');
+require_once('../DAO/personaDAO.php');
+
+$per = new Persona();
+$perDAO = new PersonaDAO();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title> Formulario para Buscar Persona</title>
+
+	<!--<link rel="stylesheet" type="text/css" href="../estilo/fcomponentes.css">-->
+</head>
+</html>
