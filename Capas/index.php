9f94c5b71c0c42b43d25380ade07ee2b1af4e493
gilbertoalexander <alexander_96.05@outlook.com>
2018-11-22 02:28:48 -0500

    Diseño
    Diseño index.php

--- Capas/index.php
+++ Capas/index.php
@@ -3,20 +3,61 @@
 <html>
 <head>
   <title>HOSPITAL</title>
-</head>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
 <link rel="stylesheet" href="CSS/principal.css">
+<link rel="stylesheet" href="CSS/style.css">
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="#"><span class="primero"><i class="icon icon-home"></i></span>Inicio</a></li>
+        <li><a href="#"><span class="segundo"><i class="icon icon-calendar"></i></span>Nueva Cita</a>
+        <li><a href="#"><span class="tercero"><i class="icon icon-user-check"></i></span>Citas Pendientes</a>
+        <li><a href="#"><span class="cuarto"><i class="icon icon-user-plus"></i></span>Nuevo Registro</a>
+          <ul>
+            <li><a href="#">Doctor</a></li>
+            <li><a href="#">Empleado</a></li>
+            <li><a href="#">Paciente</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><span class="quinto"><i class="icon icon-profile"></i></span>Registros</a>
+            <ul>
+            <li><a href="#">Doctor</a></li>
+            <li><a href="#">Empleado</a></li>
+            <li><a href="#">Paciente</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><span class="sexto"><i class="icon icon-question"></i></span>Ayuda</a></li>
+      </ul>
+    </nav>
+  </header>
+      <div class="wrap">
+    <div class="widget">
+      <div class="fecha">
+        <p id="diaSemana" class="diaSemana"></p>
+        <p id="dia" class="dia"></p>
+        <p>de </p>
+        <p id="mes" class="mes"></p>
+        <p>del </p>
+        <p id="year" class="year"></p>
+      </div>
 
-<header>
-<nav class="menu">
-    <ul>
-        <li><a href="DESIGNER/frmPersona.php">Persona</a></li>
-        <li><a href=""><i class=""></i>Inicio</a></li>
-        <li><a href=""><i class=""></i>Lista</a></li>
-        <li><a href=""><i class=""></i>Nuevo Registro</a></li>
-        <li><a href=""><i class=""></i>Nueva Cita</a></li>
-        <li><a href=""><i class=""></i>Ayuda</a></li>
-    </ul>
-</nav>
-</header>
+      <div class="reloj">
+        <p id="horas" class="horas"></p>
+        <p>:</p>
+        <p id="minutos" class="minutos"></p>
+        <p>:</p>
+        <div class="caja-segundos">
+          <p id="ampm" class="ampm"></p>
+          <p id="segundos" class="segundos"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+<script src="JS/relojes.js"></script>
+</body>
+
 
 </html>
