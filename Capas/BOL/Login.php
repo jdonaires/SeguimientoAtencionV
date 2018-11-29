efc46f5ff9f318ae9d40a5025923b3e3a55442ad
Anthony Campos <acamposch97@gmail.com>
2018-11-29 16:56:02 -0500

    creacion de login en php
    modificado

---
+++ Capas/BOL/Login.php
@@ -0,0 +1,17 @@
+<?php
+class login
+{
+	private $usuario;
+	private $contrasenia;
+
+
+	public function __GET($x)
+	{
+		return $this->$x;
+	}
+	public function __SET($x, $y)
+	{
+		return $this->$x = $y;
+	}
+}
+?>
