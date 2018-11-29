0415d56ea5f885e0accdef19f6f9abf6d6797653
Brayamn <kaname268@gmail.com>
2018-11-29 18:27:08 -0500

    Historial de citas
    correccion de  los datos de  historial

--- Capas/DAO/HistorialPacienteDAO0.php
+++ Capas/DAO/HistorialPacienteDAO0.php
@@ -17,16 +17,15 @@ public function insertarPaciente(empleado $paciente, persona $persona)
         try {
             $statement = $this->pdo->prepare("call up_insertar_persona_empleados(?,?,?,?,?,?,?,?,?,?,?)");
 
-            $statement->bindValue(1,  $persona->__GET('nombres'));
+            $statement->bindValue(1,  $persona->__GET('nombres del paciente'));
             $statement->bindValue(2,  $persona->__GET('apellidos'));
             $statement->bindValue(3,  $persona->__GET('dni'));
             $statement->bindValue(4,  $persona->__GET('direccion'));
             $statement->bindValue(5,  $persona->__GET('fecnac'));
-            $statement->bindValue(6,  $persona->__GET('genero'));
-            $statement->bindValue(7,  $persona->__GET('email'));
+            $statement->bindValue(5,  $persona->__GET('fecAtencion'));
             $statement->bindValue(8,  $persona->__GET('telefono'));
-            $statement->bindValue(9,  $persona->__GET('estado'));
-            $statement->bindValue(10, $persona->__GET('tipo'));
+            $statement->bindValue(9,  $persona->__GET(' informe estado actual '));
+            $statement->bindValue(10, $persona->__GET('Analisis Realizados'));
 
             $statement->bindValue(11,  $paciente->__GET('nrohistoria'));
 
